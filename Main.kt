c8bb604e85dd991a36b8f7fbcb938c32c116ae91
vition vlad <48290199+VitionVlad@users.noreply.github.com>
2022-10-16 22:27:59 +0300

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -320,8 +320,9 @@ class Render(size: IVec2, mesh: Array<Mesh>): JPanel(){
     }
 }
 
-class Window(title: String, size: IVec2, mesh: Array<Mesh>): JFrame(){
+class Window(title: String, useOpenGL: String, size: IVec2, mesh: Array<Mesh>): JFrame(){
     init{
+        System.setProperty("sun.java2d.opengl", useOpenGL)
         createWindow(title, size, mesh)
     }
     private fun createWindow(wtitle: String,  size: IVec2, mesh: Array<Mesh>){
@@ -337,10 +338,11 @@ fun main(args: Array<String>) {
     var objfile: ObjReader = ObjReader()
     objfile.path = "/home/vlad/IdeaProjects/KTExperimets/src/main/resources/test.obj"
     objfile.readObj()
-    var mesh = arrayOf(objfile.readObj())
+    var mesh = arrayOf(objfile.readObj(), objfile.readObj())
     mesh[0].Color.x = 200
+    mesh[1].RenderWired = true
     var size = IVec2()
     size.x = 800
     size.y = 600
-    var window = Window("Kotlin Render", size, mesh)
+    var window = Window("Kotlin Render", "true", size, mesh)
 }
